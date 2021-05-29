70ad3b6c2b106b037b6314defb97c815dd8dd853
GemmaTuron <68018621+GemmaTuron@users.noreply.github.com>
2021-05-29 12:21:39 +0200

    streamlit app mod

--- app.py
+++ app.py
@@ -7,8 +7,17 @@ from rdkit.Chem.Descriptors import ExactMolWt
 
 st.title("Filter new molecules by score")
 
-df = pd.read_csv("data/processed_results/cleaned1.csv")
-df=df.round({"total_score":3})
+@st.cache(allow_output_mutation=True)
+def download_dataset():
+    """Loads once then cached for subsequent runs"""
+    df = pd.read_csv("data/processed_results/cleaned1.csv")
+    df=df.round({"total_score":3})
+    return df
+
+# Copy the dataset so any changes are not applied to the original cached version
+df = download_dataset().copy()
+
+
 score = st.slider(
     label="Show compounds with score over",
     min_value=0.,
@@ -18,8 +27,8 @@ score = st.slider(
 )
 
 df_result = df[df["total_score"] > score]
-st.write(df_result)
+st.write("### Series 4 derived compounds: eos-#batch-#compound", df_result)
 
 
-raw_html = mols2grid.display(df_result, subset=["eosID","img", "total_score"], tooltip=["eosID", "SMILES","InchiKey", "MW"], selection=False)._repr_html_()
-components.html(raw_html, width=900, height=900, scrolling=True)
+raw_html = mols2grid.display(df_result.head(100), subset=["eosID","img", "total_score"], tooltip=["eosID", "SMILES","InchiKey", "MW"], selection=False, n_cols=7)._repr_html_()
+components.html(raw_html, width=1250, height=1250, scrolling=True)
