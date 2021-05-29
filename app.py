a0ede9a0443c4507e90e9eddd70921c22fabd8c8
GemmaTuron <68018621+GemmaTuron@users.noreply.github.com>
2021-05-29 12:47:09 +0200

    streamlit app mod

--- app.py
+++ app.py
@@ -5,12 +5,12 @@ import streamlit.components.v1 as components
 from rdkit import Chem
 from rdkit.Chem.Descriptors import ExactMolWt
 
-st.title("Filter new molecules by score")
+st.title("Series4 new proposed compounds")
 
 @st.cache(allow_output_mutation=True)
 def download_dataset():
     """Loads once then cached for subsequent runs"""
-    df = pd.read_csv("data/processed_results/cleaned1.csv")
+    df = pd.read_csv("data/processed_results/all_batches.csv")
     df=df.round({"total_score":3})
     return df
 
@@ -18,7 +18,7 @@ def download_dataset():
 df = download_dataset().copy()
 
 
-score = st.slider(
+Score = st.sidebar.slider(
     label="Show compounds with score over",
     min_value=0.,
     max_value=1.,
@@ -26,9 +26,26 @@ score = st.slider(
     step=0.01,
 )
 
-df_result = df[df["total_score"] > score]
-st.write("### Series 4 derived compounds: eos-#batch-#compound", df_result)
+Activity = st.sidebar.slider(
+    label="Show compounds with activity over",
+    min_value=0.,
+    max_value=1.,
+    value=0.6,
+    step=0.01,
+)
 
 
-raw_html = mols2grid.display(df_result.head(100), subset=["eosID","img", "total_score"], tooltip=["eosID", "SMILES","InchiKey", "MW"], selection=False, n_cols=5)._repr_html_()
-components.html(raw_html, width=1000, height=1000, scrolling=True)
+MW = st.sidebar.slider(
+    label="Show compounds with molecular weight over",
+    min_value=0.,
+    max_value=1.,
+    value=0.6,
+    step=0.01,
+)
+
+df_result = df[(df["total_score"] > Score) & (df["MW"] > MW) & (df["ActivityRegression"] > Activity)]
+st.write("### EOS-#batch-#compound", df_result)
+
+
+raw_html = mols2grid.display(df_result.head(100), subset=["eosID","img", "total_score"], tooltip=["eosID", "SMILES","InchiKey", "MW"], selection=False, n_cols=4)._repr_html_()
+components.html(raw_html, width=900, height=900, scrolling=True)
