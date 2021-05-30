128b51d52941d3aeba2ba31b570497f92fb29d55
GemmaTuron <68018621+GemmaTuron@users.noreply.github.com>
2021-05-30 20:52:53 +0200

    streamlit app

--- app.py
+++ app.py
@@ -1,24 +1,29 @@
 import mols2grid
 import pandas as pd
 import streamlit as st
+import numpy as np
 import streamlit.components.v1 as components
 from rdkit import Chem
 from rdkit.Chem.Descriptors import ExactMolWt
+st.set_page_config(layout="wide")# force wide display
+st.title("Series4 new candidates")
 
-st.title("Series4 new proposed compounds")
-
-@st.cache(allow_output_mutation=True)
-def download_dataset():
+@st.cache
+def load_data():
     """Loads once then cached for subsequent runs"""
-    df = pd.read_csv("data/ReinventResults/ProcessedResults/all_batches.csv")
-    df=df.round({"total_score":3})
+    df = pd.read_csv("data/ReinventResults/ProcessedResults/sample.csv")
     return df
 
-# Copy the dataset so any changes are not applied to the original cached version
-df = download_dataset().copy()
+df=load_data()
+
+df=df.round(3) #round numbers to 3 decimals
 
 
-Score = st.sidebar.slider(
+col1, col2=st.beta_columns((1,3)) #create two columns, for slider optins (col1) and table (col2)
+
+col1.header("Search by:")
+
+Score = col1.slider(
     label="Show compounds with score over",
     min_value=0.,
     max_value=1.,
@@ -26,7 +31,7 @@ Score = st.sidebar.slider(
     step=0.01,
 )
 
-Activity = st.sidebar.slider(
+Activity = col1.slider(
     label="Show compounds with activity over",
     min_value=0.,
     max_value=1.,
@@ -35,17 +40,33 @@ Activity = st.sidebar.slider(
 )
 
 
-MW = st.sidebar.slider(
+MW = col1.slider(
     label="Show compounds with molecular weight over",
     min_value=200.,
     max_value=800.,
+    value=300.,
+    step=0.01,
+)
+
+RA = col1.slider(
+    label="Show compounds with RA score over",
+    min_value=0.,
+    max_value=1.,
     value=0.6,
     step=0.01,
 )
 
-df_result = df[(df["total_score"] > Score) & (df["MW"] > MW) & (df["ActivityRegression"] > Activity)]
-st.write("### EOS-#batch-#compound", df_result)
+col2.header("EOS - #batch - #compound")
+            
+df_result = df[(df["TotalScore"] > Score) & (df["MolWeight"] > MW) & (df["AvActivity"] > Activity) & (df["RAScore"] > RA)]
+col2.dataframe(df_result,width=None, height=600)
 
 
-raw_html = mols2grid.display(df_result.head(100), subset=["eosID","img", "total_score"], tooltip=["eosID", "SMILES","InchiKey", "MW"], selection=False, n_cols=4)._repr_html_()
-components.html(raw_html, width=900, height=900, scrolling=True)
+
+st.subheader("Molecular Structures")
+raw_html = mols2grid.display(df_result.head(100), subset=["eosID","img", "TotalScore"], tooltip=["eosID", "SMILES","InchiKey", "AvActivity"], selection=False, n_cols=6)._repr_html_()
+components.html(raw_html,height=600, scrolling=True)
+
+st.subheader("Molecular weight distribution")
+hist_values=np.histogram(df["MolWeight"])
+st.bar_chart(hist_values)
